839f554f56eb93b8a3085fa783f92fcba09d6c47
Genaro Gonzalez <genarrogg@gmail.com>
2025-03-23 11:43:50 -0400

    up

--- functions/server.js
+++ functions/server.js
@@ -4,11 +4,11 @@ const app = fastify();
 const serverless = require("serverless-http");
 
 app.get("/test", (request, reply) => {
-  reply.send({ hello: "world" });
+  reply.send({ hello: "world2" });
 });
 
 app.get("/test/one", (request, reply) => {
-  reply.send({ hello: "world" });
+  reply.send({ hello: "world2" });
 });
 
 module.exports.handler = serverless(app);
